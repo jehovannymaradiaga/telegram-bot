ee2508be838bc1bfaf236d8b3ecf111908979f02
JehovannyMaradiaga <maradiagawalter6@gmail.com>
2025-04-20 19:13:02 -0600

    Creación basica del bot de telegrama para una tienda de zapatos

--- src/catalogo.py
+++ src/catalogo.py
@@ -1,50 +1,50 @@
 catalogo = {
     "photo1": {
-        "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/zapato_clasico1.jpg",
+        "ruta": "../images/zapato_clasico1.jpg",
         "descripcion": "Zapatilla Deportiva\n\nPrecio: $55"
     },
     "photo2": {
-        "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/zapato_clasico2.jpg",
+        "ruta": "../images/zapato_clasico2.jpg",
         "descripcion": "Zapatilla Deportiva\n\nPrecio: $74"
     },
     "photo3": {
-        "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/zapato_clasico3.jpg",
+        "ruta": "../images/zapato_clasico3.jpg",
         "descripcion": "Zapatilla Deportiva\n\nPrecio: $52"
     },
     "photo4": {
-    "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/zapatilla_deportiva1.png",
+    "ruta": "../images/zapatilla_deportiva1.png",
     "descripcion": "Zapatilla Deportiva\n\nPrecio: $20"
     },
     "photo5": {
-        "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/zapatilla_deportiva2.jpg",
+        "ruta": "../images/zapatilla_deportiva2.jpg",
         "descripcion": "Zapatilla Deportiva\n\nPrecio: $25"
     },
     "photo6": {
-        "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/zapatilla_deportiva3.jpg",
+        "ruta": "../images/zapatilla_deportiva3.jpg",
         "descripcion": "Zapatilla Deportiva\n\nPrecio: $15"
     },
     "photo7": {
-        "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/tenis_oferta1.jpg",
+        "ruta": "../images/tenis_oferta1.jpg",
         "descripcion": "Zapatilla Deportiva\n\nPrecio Antes: $20 Ahora: $12"
     },
     "photo8": {
-        "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/tenis_oferta2.jpg",
+        "ruta": "../images/tenis_oferta2.jpg",
         "descripcion": "Zapatilla Deportiva\n\nPrecio: Antes $25 Ahora $18"
     },
     "photo9": {
-        "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/tenis_oferta3.jpg",
+        "ruta": "../images/tenis_oferta3.jpg",
         "descripcion": "Zapatilla Deportiva\n\nPrecio: Antes $15 Ahora $10"
     },
     "photo10": {
-        "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/tenis_oferta4.jpg",
+        "ruta": "../images/tenis_oferta4.jpg",
         "descripcion": "Zapatilla Deportiva\n\nPrecio: Antes $55 Ahora $43"
     },
     "photo11": {
-        "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/tenis_oferta5.jpg",
+        "ruta": "../images/tenis_oferta5.jpg",
         "descripcion": "Zapatilla Deportiva\n\nPrecio: Antes $74 Ahora $50"
     },
     "photo12": {
-        "ruta": "C:/Users/50495/OneDrive/Escritorio/JM_Shoes_Bot/images/tenis_oferta6.jpg",
+        "ruta": "../images/tenis_oferta6.jpg",
         "descripcion": "Zapatilla Deportiva\n\nPrecio:Antes  $52 Ahora $44"
     },
 }
